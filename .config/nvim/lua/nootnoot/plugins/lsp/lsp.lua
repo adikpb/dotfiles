f9ac2c371128b9ed507387ee32bac3fd54674c36
adikpb <67222969+adikpb@users.noreply.github.com>
2025-04-30 20:52:17 +0530

    nvim(fix: lsp): fix server configure merge

--- .config/nvim/lua/nootnoot/plugins/lsp/lsp.lua
+++ .config/nvim/lua/nootnoot/plugins/lsp/lsp.lua
@@ -93,12 +93,12 @@ return {
           if server_name == "ruff" then
             return
           end
-          local server = servers[server_name] or {}
+          local server_config = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
           -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend("force", {}, server.capabilities or {}, capabilities)
-          vim.lsp.config(server_name, server)
+          server_config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server_config.capabilities or {})
+          vim.lsp.config(server_name, server_config)
           vim.lsp.enable(server_name)
         end,
       },
