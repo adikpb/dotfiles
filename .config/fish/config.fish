56922585406fb2f7a1d7b33df8c2e3451c6d8868
adikpb <67222969+adikpb@users.noreply.github.com>
2025-05-04 20:20:02 +0530

    fix(fish): Only run source commands in interactive

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,17 +3,19 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 ## change this - java home
 set -gx JAVA_HOME (/usr/libexec/java_home)
 
-# starship
-starship init fish | source
-
 # cargo
 fish_add_path ~/.cargo/bin
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-# https://docs.astral.sh/uv/getting-started/installation/#shell-autocompletion
-uv generate-shell-completion fish | source
-uvx --generate-shell-completion fish | source
-
 # uv
 fish_add_path "/Users/bijoykozhampurath/.local/bin"
+
+if status is-interactive
+    # starship
+    starship init fish | source
+
+    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+    # https://docs.astral.sh/uv/getting-started/installation/#shell-autocompletion
+    uv generate-shell-completion fish | source
+    uvx --generate-shell-completion fish | source
+end
