2023a951296cf7e97b56a485f7b7270f544c3aa9
adikpb <67222969+adikpb@users.noreply.github.com>
2025-10-05 15:49:01 +0530

    format(nvim: lazy_init)

--- .config/nvim/lua/nootnoot/core/lazy_init.lua
+++ .config/nvim/lua/nootnoot/core/lazy_init.lua
@@ -1,29 +1,30 @@
 -- https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ import = "nootnoot.plugins" },
-	{ import = "nootnoot.plugins.lsp" },
-	{ import = "nootnoot.plugins.editing-support" },
-	{ import = "nootnoot.plugins.git" },
-	{ import = "nootnoot.plugins.theming" },
-	{ import = "nootnoot.plugins.utility" },
+  { import = "nootnoot.plugins" },
+  { import = "nootnoot.plugins.debugging" },
+  { import = "nootnoot.plugins.editing-support" },
+  { import = "nootnoot.plugins.git" },
+  { import = "nootnoot.plugins.lsp" },
+  { import = "nootnoot.plugins.theming" },
+  { import = "nootnoot.plugins.utility" },
 }, {
-	change_detection = { notify = false },
-	defaults = { lazy = true },
-	dev = {
-		path = "~/neovim-plugins",
-		fallback = true,
-	},
+  change_detection = { notify = false },
+  defaults = { lazy = true },
+  dev = {
+    path = "~/neovim-plugins",
+    fallback = true,
+  },
 })
