3c5b75a4cbd90ab58d25a9d304a3dd9605d3ad7f
adikpb <67222969+adikpb@users.noreply.github.com>
2025-11-11 00:05:44 +0530

    fix(nvim): load opt rtp plugins after VimEnter

--- .config/nvim/lua/core/rtp_plugins.lua
+++ .config/nvim/lua/core/rtp_plugins.lua
@@ -1,2 +1,10 @@
-vim.cmd("packadd nvim.undotree")
-vim.keymap.set("n", "<leader>u", "<cmd>Undotree<cr>")
+-- load after rtp's has been setup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function(_)
+    -- nvim.undotree
+    vim.cmd("packadd nvim.undotree")
+    vim.keymap.set("n", "<leader>u", function()
+      require("undotree").open({ command = "rightbelow 30vnew" })
+    end)
+  end,
+})
