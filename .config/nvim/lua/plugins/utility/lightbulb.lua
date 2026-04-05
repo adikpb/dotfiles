783b271f4429f8e3b9dafbe320314e9672c80890
adikpb <67222969+adikpb@users.noreply.github.com>
2026-04-05 08:47:49 +0530

    feat(nvim: lightbulb): enable codelens

--- .config/nvim/lua/plugins/utility/lightbulb.lua
+++ .config/nvim/lua/plugins/utility/lightbulb.lua
@@ -3,10 +3,8 @@ return {
   event = "LspAttach",
   opts = {
     autocmd = { enabled = true },
-    ignore = {
-      clients = {},
-      ft = {},
-      actions_without_kind = false,
-    },
+    code_lenses = true,
+    sign = {enabled = false},
+    float = {enabled = true}
   },
 }
