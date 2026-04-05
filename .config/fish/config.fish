f9f176a4e219355a342b55a810cf026286de9efd
adikpb <67222969+adikpb@users.noreply.github.com>
2026-04-05 08:53:07 +0530

    chore(fish): update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,25 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-fish_add_path -g "/usr/local/bin"
-
+set -g fish_greeting ""
+set -gx EDITOR "nvim"
 ## change this - java home
 set -gx JAVA_HOME (/usr/libexec/java_home)
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+fish_add_path -g "/usr/local/bin"
 # rustup
 fish_add_path -g "$(brew --prefix rustup)/bin"
-
 # uv
 fish_add_path -g "/Users/bijoykozhampurath/.local/bin"
+# Added by LM Studio CLI (lms)
+fish_add_path -g /Users/bijoykozhampurath/.lmstudio/bin
+# bun
+set -gx BUN_INSTALL "$HOME/.bun"
+fish_add_path -g $BUN_INSTALL/bin $PATH
 
 if status is-interactive
     starship init fish | source
     fzf --fish | source
     zoxide init fish | source
     sesh completion fish | source
-
-    # https://docs.astral.sh/uv/getting-started/installation/#shell-autocompletion
     uv generate-shell-completion fish | source
     uvx --generate-shell-completion fish | source
 end
