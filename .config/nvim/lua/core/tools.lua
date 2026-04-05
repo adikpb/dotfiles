ab28ed044603a8252dad7205eacd7c2ead30b36c
adikpb <67222969+adikpb@users.noreply.github.com>
2026-04-05 09:02:58 +0530

    refactor(nvim): use string instead of k,v pairs

--- .config/nvim/lua/core/tools.lua
+++ .config/nvim/lua/core/tools.lua
@@ -1,25 +1,27 @@
 LSP_SERVERS = {
-  basedpyright = {},
-  ruff = {},
-  clangd = {},
-  jdtls = {},
-  lua_ls = {},
-  marksman = {},
-  rust_analyzer = {},
-  taplo = {},
+  "basedpyright",
+  "ruff",
+  "clangd",
+  "jdtls",
+  "lua_ls",
+  "marksman",
+  "rust_analyzer",
+  "taplo",
 }
 
 local CODE_FORMATTERS = {
-  stylua = {},
-  ruff = {},
-  ["clang-format"] = {},
-  prettier = {},
+  "stylua",
+  "ruff",
+  "clang-format",
+  "prettier",
 }
 
 local TOOLS = { mmdc = {} } -- not available in brew
-TOOLS = vim.tbl_keys(vim.tbl_deep_extend("force", TOOLS, LSP_SERVERS, CODE_FORMATTERS))
 
 local MANUALLY_INSTALLED_TOOLS = {}
+
+TOOLS = vim.tbl_keys(vim.tbl_deep_extend("force", TOOLS, LSP_SERVERS, CODE_FORMATTERS))
+
 
 ENSURE_INSTALLED = vim.tbl_filter(function(name)
   return not vim.tbl_contains(MANUALLY_INSTALLED_TOOLS, name)
