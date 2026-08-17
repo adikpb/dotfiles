52d4027e7794784a6af82533d8b7290a99c606d6
adikpb <67222969+adikpb@users.noreply.github.com>
2026-08-17 06:09:43 +0400

    feat(nvim: lightbulb): use virtual text

--- .config/nvim/lua/plugins/utility/lightbulb.lua
+++ .config/nvim/lua/plugins/utility/lightbulb.lua
@@ -1,10 +1,12 @@
 return {
   "kosayoda/nvim-lightbulb",
   event = "LspAttach",
+  ---@module "nvim-lightbulb"
+  ---@type nvim-lightbulb.Config
   opts = {
     autocmd = { enabled = true },
     code_lenses = true,
     sign = {enabled = false},
-    float = {enabled = true}
+    virtual_text = {enabled = true}
   },
 }
